339da61cd266964550b43e6d6e8af643d9fd6321
AyseYuksel <68689527+AyseYuksel@users.noreply.github.com>
2021-03-16 16:10:36 +0300

    initial commit

---
+++ biSection.m
@@ -0,0 +1,24 @@
+﻿
+%for example :[m,error]=biSection('x-2',1,4,10)
+
+function [m,err]=biSection(func,a,b,n)
+
+str1='@(x)';
+str2=func;
+str=[str1 str2];
+f=str2func(str);
+
+if f(a).*f(b)<=0
+    for i=0:n
+        m=(a+b)/2;
+        err=abs(f(m));
+     %  disp([m err]);
+        if f(a).*f(m)<=0
+            b=m;
+        else
+            a=m;
+        end
+    end
+else
+    disp("function does not have a root");
+end
